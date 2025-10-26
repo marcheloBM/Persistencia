0b624972fc16899235fefeb8288c7e4849700372
Marcelo Burgos <marchelo.bm@gmail.com>
2025-10-26 02:15:34 -0300

    v2.2.2 Cambios
    cambios para el APK

--- Persistencia/app/src/androidTest/java/cl/duoc/android/persistencia/ApplicationTest.java
+++ Persistencia/app/src/androidTest/java/cl/duoc/android/persistencia/ApplicationTest.java
@@ -1,13 +1,34 @@
 package cl.duoc.android.persistencia;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        // Pobierz kontekst aplikacji, która jest testowana.
+        Context appContext = ApplicationProvider.getApplicationContext();
+
+        // Przykład asercji: sprawdź, czy nazwa pakietu jest poprawna.
+        assertEquals("cl.duoc.android.persistencia", appContext.getPackageName());
+    }
+
+    @Test
+    public void application_isNotNull() {
+        // Możesz również uzyskać instancję samej aplikacji.
+        Application application = ApplicationProvider.getApplicationContext();
+        assertNotNull(application);
     }
 }
